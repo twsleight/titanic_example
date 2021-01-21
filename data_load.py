fab686478fa5422abbfac79eb1ad6188763a98df
lmarie7309311 <77786144+lmarie7309311@users.noreply.github.com>
2021-01-21 12:06:28 -0500

    Sort data by survivability

--- data_load.py
+++ data_load.py
@@ -10,3 +10,4 @@ import os
 train_data = pd.read_csv(os.path.join(os.path.dirname(__file__), 'train.csv') )
 test_data = pd.read_csv(os.path.join(os.path.dirname(__file__), 'test.csv') )
 
+train_data.sort_values(by = 'Survived', inplace = True)
